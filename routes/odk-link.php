aee2dc6fe59f49553e3a3df300ac42fdfe612a07
Dan Tang <dan@stats4sd.org>
2023-01-10 09:53:34 +0000

    Add new routes for Select button and submitSelectedFields

--- routes/odk-link.php
+++ routes/odk-link.php
@@ -18,8 +18,11 @@ Route::group([
 
     Route::crud('xlsform-template', XlsformTemplateCrudController::class);
 
-    // Add a new route for XlsForm Template CRUD list view "Select" button
+    // Add a new route for XlSForm Template CRUD list view "Select" button
     Route::get('xlsform-template/{id}/select', [XlsformTemplateCrudController::class, 'select']);
+
+    // Add a new route to handle the form submission for user selected ODK variables
+    Route::post('xlsform-template/{id}/submitSelectedFields', [XlsformTemplateCrudController::class, 'submitSelectedFields']);
 
     Route::crud('xlsform', XlsformCrudController::class);
     Route::crud('odk-project', OdkProjectCrudController::class);
