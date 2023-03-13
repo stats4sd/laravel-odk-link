6125da23978b0e2ac4bdfe192920a8eb23225a60
ciara-mc <56551262+ciara-mc@users.noreply.github.com>
2023-03-13 16:44:36 +0000

    updates migrations

--- src/OdkLinkServiceProvider.php
+++ src/OdkLinkServiceProvider.php
@@ -26,13 +26,14 @@ class OdkLinkServiceProvider extends PackageServiceProvider
             ->hasConfigFile()
             ->hasViews()
             ->hasMigrations([
-                '1_create_xlsform_templates_table',
-                '2_create_xlsforms_table',
-                '3_create_xlsform_versions_table',
-                '4_create_submissions_table',
-                '5_create_odk_projects_table',
-                '6_create_app_users_table',
-                '7_create_app_user_assignments_table',
+                '1_create_xlsform_subjects_table',
+                '2_create_xlsform_templates_table',
+                '3_create_xlsforms_table',
+                '4_create_xlsform_versions_table',
+                '5_create_submissions_table',
+                '6_create_odk_projects_table',
+                '7_create_app_users_table',
+                '8_create_app_user_assignments_table',
             ])
             ->hasRoute('/odk-link')
             ->hasCommands([
