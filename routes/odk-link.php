4b917a685026c5692982f27f24c4e4153b83dc35
Dave Mills <d.e.mills@stats4sd.org>
2023-08-31 15:49:57 +0100

    fix-typo

--- routes/odk-link.php
+++ routes/odk-link.php
@@ -1,6 +1,6 @@
 <?php
 
-// some randome change
+// some random change
 // Admin panels from the ODK Link Package:
 use Stats4sd\OdkLink\Http\Controllers\XlsformTemplateController;
 use Stats4sd\OdkLink\Models\Xlsform;
